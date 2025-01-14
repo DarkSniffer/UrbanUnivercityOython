1314d70d66bd0c46f018ad07f9a62809cda77d05
Dmitry <snifcoder@gmail.com>
2025-01-15 00:31:14 +0500

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -1,24 +1,24 @@
 from fastapi import FastAPI
-
+from fastapi.responses import PlainTextResponse
 # Создаем объект API
 app = FastAPI()
 
 # Прописываем маршрут к главной странице
 @app.get("/")
 async def read_mainpage():
-    return {"message": "Главная страница"}
+    return PlainTextResponse('"Главная страница"')
 
 # Прописываем маршрут к странице администратора
 @app.get("/user/admin")
 async def read_adminpage():
-    return {"message": "Вы вошли как администратор"}
+    return PlainTextResponse('"Вы вошли как администратор"')
 
 # Прописываем маршрут к страницам пользователей с передачей данных в адресной строке
 @app.get("/user")
 async def read_user_info(username: str, age: str):
-    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+    return PlainTextResponse(f'\"Информация о пользователе. Имя: \'{username}\', Возраст: {age}\"')
 
 # Прописываем маршрут к страницам пользователей с параметром в пути
 @app.get("/user/{user_id}")
 async def read_user_id(user_id: str):
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
+    return PlainTextResponse(f'\"Вы вошли как пользователь № {user_id}\"')
